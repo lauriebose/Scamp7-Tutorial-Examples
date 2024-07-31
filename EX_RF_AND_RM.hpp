bb03aa0e2cb74de9db215c501005600f534883b7
lauriebose <lauriebose@gmail.com>
2024-07-31 16:18:40 +0100

    Add files via upload

--- EX_RF_AND_RM.hpp
+++ EX_RF_AND_RM.hpp
@@ -29,11 +29,17 @@ int main()
 	    int box_x, box_y, box_width, box_height;
 	    vs_gui_add_slider("box x: ", 0, 255, 128, &box_x);
 	    vs_gui_add_slider("box y: ", 0, 255, 128, &box_y);
-	    vs_gui_add_slider("box width: ", 0, 128, 64, &box_width);
-	    vs_gui_add_slider("box height: ", 0, 128, 64, &box_height);
+	    vs_gui_add_slider("box width: ", 0, 255, 96, &box_width);
+	    vs_gui_add_slider("box height: ", 0, 255, 96, &box_height);
 
 		int threshold_value = 64;
 		vs_gui_add_slider("threshold_value",-127,127,threshold_value,&threshold_value);
+
+		int clear_RM = 0;
+		vs_gui_add_switch("clear_RM",clear_RM == 1, &clear_RM);
+
+		int refresh_RM = 0;
+		vs_gui_add_switch("refresh_RM",refresh_RM == 1, &refresh_RM);
 
 
     //CONTINOUS FRAME LOOP
@@ -68,28 +74,38 @@ int main()
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //EXAMPLE OF PERFORMING "FLAGGED" DIGITAL OPERATIONS WITH RF & RM
 
+			if(clear_RM)
+			{
+				scamp7_kernel_begin();
+					//Set RF=1 in all PEs
+					SET(RF);
+						//Set RM=0 in all PEs where RF=1 (currently all of them!)
+						CLR(RM);
+				scamp7_kernel_end();
+			}
+
+
 			//Load a rectangular region into S6, this will be copied to RF later
 			DREG_load_centered_rect(S6,box_x,box_y,box_width,box_height);
 
 			//All digital operations targeting RM are flagged by RF
 			//I.E. when a digital operation is performed that would change the content of RM, it is only performed in PEs where RF = 1
 			scamp7_kernel_begin();
-
-				//Set RF=1 in all PEs
-				SET(RF);
-					//Set RM=0 in all PEs where RF=1 (currently all of them!)
-					CLR(RM);
-
 				//Copy rectangular region in S6 into FLAG
 				MOV(RF,S6);
 
 				    //Copy thresholded image, stored in S0, into RM.
 				    //Will only be performed in PEs where RF=1 (i.e. only inside the rectangle)
 					MOV(RM,S0);
-
-				//Copy result of flagged digital operation into S5
-				MOV(S5,RM);
 			scamp7_kernel_end();
+
+			if(refresh_RM)
+			{
+				scamp7_kernel_begin();
+					SET(RF);
+						REFRESH(RM);
+				scamp7_kernel_end();
+			}
 
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -98,7 +114,7 @@ int main()
 			output_timer.reset();
 			scamp7_output_image(S0,display_00);
 			scamp7_output_image(S6,display_01);
-			scamp7_output_image(S5,display_02);
+			scamp7_output_image(RM,display_02);
 			int output_time_microseconds = output_timer.get_usec();//get the time taken for image output
 
 	    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
